5f5fb88d3a52f29ec81906618a8206ec4baf0bd1
Shashidhara Meti <shashimeti75@gmail.com>
2025-08-02 09:49:07 +0530

    Use secrets.toml for credentials

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ else:
                     - **Brand**: `{row['Brand']}`
                     - **Vertical**: `{vertical}`
                     - **Available Qty**: `{row['Quantity']}`
-                    - **status**: `{row['Status']}`
+                    - **status**: `{row['status']}`
                     """)
                     counted = st.number_input("Enter Counted Quantity", min_value=0, step=1)
 
